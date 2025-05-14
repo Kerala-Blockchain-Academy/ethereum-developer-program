9009ae258cf38d8489109dde245cda96d3ab5dbe
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-05-14 11:27:09 +0530

    🪛 | update snippets/ethers
    Signed-off-by: tr1sm0s1n <mobinm.mohanan@gmail.com>

--- snippets/ethers/index.mjs
+++ snippets/ethers/index.mjs
@@ -1,55 +1,58 @@
-import { readFileSync } from "fs";
-import solc from "solc";
-import { JsonRpcProvider, ContractFactory } from "ethers";
+import { readFileSync } from 'fs'
+import solc from 'solc'
+import { JsonRpcProvider, ContractFactory } from 'ethers'
 
-const provider = new JsonRpcProvider("http://127.0.0.1:8545");
+const provider = new JsonRpcProvider('http://127.0.0.1:8545')
 
-const signer = await provider.getSigner();
-console.log("Address:", signer.address);
+const signer = await provider.getSigner()
+console.log('Address:', signer.address)
 
-const balance = await provider.getBalance(signer.address);
-console.log("Balance:", balance);
+const balance = await provider.getBalance(signer.address)
+console.log('Balance:', balance)
 
-const network = await provider.getNetwork();
-console.log("Chain ID:", network.chainId);
+const network = await provider.getNetwork()
+console.log('Chain ID:', network.chainId)
 
-let latestBlock = await provider.getBlockNumber();
-console.log("Latest Block:", latestBlock);
+let latestBlock = await provider.getBlockNumber()
+console.log('Current Block:', latestBlock)
 
-const CONTRACT_FILE = "./../common/contracts/Storage.sol";
+const CONTRACT_FILE = './../common/contracts/Storage.sol'
 
-const content = readFileSync(CONTRACT_FILE).toString();
+const content = readFileSync(CONTRACT_FILE).toString()
 
 const input = {
-    language: "Solidity",
-    sources: {
-        [CONTRACT_FILE]: {
-            content: content,
-        },
+  language: 'Solidity',
+  sources: {
+    [CONTRACT_FILE]: {
+      content: content,
     },
-    settings: {
-        outputSelection: {
-            "*": {
-                "*": ["*"],
-            },
-        },
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*'],
+      },
     },
-};
+  },
+}
 
-const compiled = solc.compile(JSON.stringify(input));
-const output = JSON.parse(compiled);
+const compiled = solc.compile(JSON.stringify(input))
+const output = JSON.parse(compiled)
 
-const abi = output.contracts[CONTRACT_FILE].Storage.abi;
-const bytecode = output.contracts[CONTRACT_FILE].Storage.evm.bytecode.object;
+const abi = output.contracts[CONTRACT_FILE].Storage.abi
+const bytecode = output.contracts[CONTRACT_FILE].Storage.evm.bytecode.object
 
-const factory = new ContractFactory(abi, bytecode, signer);
-const contract = await factory.deploy();
+const factory = new ContractFactory(abi, bytecode, signer)
+const contract = await factory.deploy()
 
-const trx = await contract.store("Hello, KBA!");
-console.log("Transaction Hash:", trx.hash);
+const deployTrx = contract.deploymentTransaction()
+console.log('Deployment Tx Hash:', deployTrx.hash)
 
-const message = await contract.retrieve();
-console.log("Message:", message);
+const trx = await contract.store('This message is stored using ethers.')
+console.log('Storage Tx Hash:', trx.hash)
 
-latestBlock = await provider.getBlockNumber();
-console.log("Latest Block:", latestBlock);
+const message = await contract.retrieve()
+console.log('Message:', message)
+
+latestBlock = await provider.getBlockNumber()
+console.log('Latest Block:', latestBlock)
