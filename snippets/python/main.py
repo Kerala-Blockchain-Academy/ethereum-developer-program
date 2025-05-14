e97afba5a3c25c8710e63c20b9f3fe431992f35e
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-05-14 15:17:59 +0530

    🐍 | add Python snippet for deployment and interaction
    Signed-off-by: tr1sm0s1n <mobinm.mohanan@gmail.com>

---
+++ snippets/python/main.py
@@ -0,0 +1,45 @@
+import json
+from web3 import Web3
+from eth_account import Account
+
+
+def main():
+    w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+
+    with open("../common/contracts/Storage.json", "r", -1, "utf-8") as a:
+        abi = json.load(a)
+
+    with open("../common/contracts/Storage.bin", "r", -1, "utf-8") as b:
+        bytecode = b.read()
+
+    account = Account.from_key(
+        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+    )
+
+    print(f"Address: \033[32m{account.address}\033[0m")
+    print(f"Balance: \033[33m{w3.eth.get_balance(account.address)}\033[0m")
+    print(f"Chain ID: \033[33m{w3.eth.chain_id}\033[0m")
+    print(f"Current Block: \033[33m{w3.eth.get_block_number()}\033[0m")
+
+    contract = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+    deploy_tx = contract.constructor().transact()
+    tx_receipt = w3.eth.wait_for_transaction_receipt(deploy_tx)
+    print(f"Deployment Tx Hash: \033[32m{'0x'+tx_receipt.transactionHash.hex()}\033[0m")
+
+    instance = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+
+    store_tx = instance.functions.store(
+        "This message is stored using Python."
+    ).transact()
+    tx_receipt = w3.eth.wait_for_transaction_receipt(store_tx)
+    print(f"Storage Tx Hash: \033[32m{'0x'+tx_receipt.transactionHash.hex()}\033[0m")
+
+    value = instance.functions.retrieve().call()
+    print(f"Message: \033[36m{value}\033[0m")
+
+    print(f"Latest Block: \033[33m{w3.eth.get_block_number()}\033[0m")
+
+
+if __name__ == "__main__":
+    main()
